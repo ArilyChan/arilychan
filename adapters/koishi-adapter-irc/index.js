682d81b7e9eb2bd60ae6bca859308ec138c65233
arily <lbsaaaaa@gmail.com>
2021-03-09 23:56:17 +0900

    lint

--- adapters/koishi-adapter-irc/index.js
+++ adapters/koishi-adapter-irc/index.js
@@ -1,186 +1,184 @@
-const { Adapter, Bot, Session, segment: s } = require("koishi-core");
+const { Adapter, Bot, Session, segment: s } = require('koishi-core')
 
-const irc = require("irc");
-const pEvent = require("p-event");
+const irc = require('irc')
+const pEvent = require('p-event')
 
 class IRCBot extends Bot {
-  transformSegment(seg) {
+  transformSegment (seg) {
     switch (seg.type) {
-      case "text": {
-        return seg.data.content;
+      case 'text': {
+        return seg.data.content
       }
-      case "at":
-      case "sharp": {
-        const target = seg.data.name || seg.data.id || seg.data.qq;
-        return `@${target}`;
+      case 'at':
+      case 'sharp': {
+        const target = seg.data.name || seg.data.id || seg.data.qq
+        return `@${target}`
       }
-      case "reply": {
-        const [, target] = seg.data.id.split(":");
-        return `@${target} `; // usually there's no trailing space after reply. Add it for readibility
+      case 'reply': {
+        const [, target] = seg.data.id.split(':')
+        return `@${target} ` // usually there's no trailing space after reply. Add it for readibility
       }
-      case "image": {
-        if (!seg.data.url) return "[Image(not available)]";
-        return `[${seg.data.url} Image]`;
+      case 'image': {
+        if (!seg.data.url) return '[Image(not available)]'
+        return `[${seg.data.url} Image]`
       }
-      case "audio": {
-        if (!seg.data.url) return "[Audio(not available)]";
-        return `[${seg.data.url} Audio]`;
+      case 'audio': {
+        if (!seg.data.url) return '[Audio(not available)]'
+        return `[${seg.data.url} Audio]`
       }
-      case "video": {
-        if (!seg.data.url) return "[Video(not available)]";
-        return `[${seg.data.url} Video]`;
+      case 'video': {
+        if (!seg.data.url) return '[Video(not available)]'
+        return `[${seg.data.url} Video]`
       }
-      case "file": {
-        if (!seg.data.url) return "[File(not available)]";
-        return `[${seg.data.url} File]`;
+      case 'file': {
+        if (!seg.data.url) return '[File(not available)]'
+        return `[${seg.data.url} File]`
       }
-      case "face": {
-        return `[动画表情(${seg.data.name}.jpg)]`;
+      case 'face': {
+        return `[动画表情(${seg.data.name}.jpg)]`
       }
 
       default:
-        console.log(seg);
-        return "";
+        console.log(seg)
+        return ''
     }
   }
 
-  async sendMessage(channelId, content, isPrivate = false) {
+  async sendMessage (channelId, content, isPrivate = false) {
     // this.app.logger('adapter-irc:bot').info(s.parse(content))
-    const parsed = s.parse(content);
-    const message = parsed.map(this.transformSegment);
+    const parsed = s.parse(content)
+    const message = parsed.map(this.transformSegment)
     this.app
-      .logger("adapter-irc:bot")
-      .debug("sending message", { channelId, content });
-    return this.client.say(channelId, message.join(""));
+      .logger('adapter-irc:bot')
+      .debug('sending message', { channelId, content })
+    return this.client.say(channelId, message.join(''))
   }
 
-  async sendGroupMessage(channelId, ...args) {
-    if (!channelId.startsWith("#"))
-      throw new Error("trying to send group msg to non-group channel");
-    return this.sendMessage(channelId, ...args);
+  async sendGroupMessage (channelId, ...args) {
+    if (!channelId.startsWith('#')) { throw new Error('trying to send group msg to non-group channel') }
+    return this.sendMessage(channelId, ...args)
   }
 
-  async sendPrivateMessage(channelId, ...args) {
-    if (!channelId.startsWith("#"))
-      throw new Error("trying to send private msg to non-private channel");
-    return this.sendMessage(channelId, ...args);
+  async sendPrivateMessage (channelId, ...args) {
+    if (!channelId.startsWith('#')) { throw new Error('trying to send private msg to non-private channel') }
+    return this.sendMessage(channelId, ...args)
   }
 
-  async getChannel(channelId) {
-    return this.getChannelList().find((channel) => channel.name === channelId);
+  async getChannel (channelId) {
+    return this.getChannelList().find((channel) => channel.name === channelId)
   }
 
-  async getChannelList() {
-    if (this.list) return this.list;
-    this.client.list();
-    const list = await pEvent(this.client, "channellist");
+  async getChannelList () {
+    if (this.list) return this.list
+    this.client.list()
+    const list = await pEvent(this.client, 'channellist')
     this.list = list.map((channel) => ({
       channelId: channel.name,
       channelName: channel.name,
-      topic: channel.topic,
-    }));
-    return list;
+      topic: channel.topic
+    }))
+    return list
   }
 
-  supportV2PluginSession(session) {
-    Object.defineProperty(session, "messageId", {
-      get() {
+  supportV2PluginSession (session) {
+    Object.defineProperty(session, 'messageId', {
+      get () {
         return {
-          toString() {
-            return `${to}:${nick}`;
+          toString () {
+            return `${to}:${nick}`
           },
           channelId: to,
-          nickname: nick,
-        };
+          nickname: nick
+        }
+      },
+      enumerable: true
+    })
+    Object.defineProperty(session, 'message', {
+      get () {
+        this.app
+          .logger('adapter-irc:bot')
+          .warn(Error('please use session.content in v3.\nStack Trace:').stack)
+        return session.content
+      },
+      set (newValue) {
+        session.content = newValue
       },
       enumerable: true,
-    });
-    Object.defineProperty(session, "message", {
-      get() {
+      configurable: true
+    })
+    Object.defineProperty(session, '$parsed', {
+      get () {
         this.app
-          .logger("adapter-irc:bot")
-          .warn(Error("please use session.content in v3.\nStack Trace:").stack);
-        return session.content;
-      },
-      set(newValue) {
-        session.content = newValue;
-      },
-      enumerable: true,
-      configurable: true,
-    });
-    Object.defineProperty(session, "$parsed", {
-      get() {
-        this.app
-          .logger("adapter-irc:bot")
-          .warn(Error("Session.$parsed is removed in v3.").stack);
+          .logger('adapter-irc:bot')
+          .warn(Error('Session.$parsed is removed in v3.').stack)
         return {
-          message: session.content.trim(),
-        };
+          message: session.content.trim()
+        }
       },
       enumerable: true,
-      configurable: true,
-    });
+      configurable: true
+    })
     session.$send = (...args) => {
       this.app
-        .logger("adapter-irc:bot")
-        .warn(Error("please use session.send() in v3.\nStack Trace:").stack);
-      return session.send(...args);
-    };
+        .logger('adapter-irc:bot')
+        .warn(Error('please use session.send() in v3.\nStack Trace:').stack)
+      return session.send(...args)
+    }
   }
 }
 
 class IRCAdapter extends Adapter {
-  constructor(app) {
+  constructor (app) {
     // MyBot 跟上面一样，我就不写了
-    super(app, IRCBot);
+    super(app, IRCBot)
   }
 
-  start() {
+  start () {
     this.bots.map(async (bot) => {
       if (bot.client) {
         try {
           // bot.client.disconnect()
-          bot.client.removeAllListeners();
+          bot.client.removeAllListeners()
           // bot.client = undefined
         } catch (error) {}
       }
       bot.client = new irc.Client(bot.host, bot.nickname, {
         ...bot,
-        userName: bot.username || bot.userName,
-      });
-      bot.selfId = bot.nickname;
-      bot.client.connect();
-      bot.client.addListener("error", function (message) {
-        bot.app.logger("adapter-irc:bot").error("error: ", message);
-      });
-      await pEvent(bot.client, "registered");
+        userName: bot.username || bot.userName
+      })
+      bot.selfId = bot.nickname
+      bot.client.connect()
+      bot.client.addListener('error', function (message) {
+        bot.app.logger('adapter-irc:bot').error('error: ', message)
+      })
+      await pEvent(bot.client, 'registered')
 
-      const channels = await bot.getChannelList();
-      channels.map((channel) => bot.client.join(channel.channelId));
-      console.table(channels);
+      const channels = await bot.getChannelList()
+      channels.map((channel) => bot.client.join(channel.channelId))
+      console.table(channels)
 
-      bot.client.on("message", (nick, to, text, message) => {
+      bot.client.on('message', (nick, to, text, message) => {
         bot.app
-          .logger("adapter-irc:bot")
-          .debug("received message", { nick, to, text });
+          .logger('adapter-irc:bot')
+          .debug('received message', { nick, to, text })
         const data = {
-          type: "message",
+          type: 'message',
           platform: bot.platform,
           selfId: bot.selfId,
-          messageType: to.startsWith("#") ? "group" : "private",
+          messageType: to.startsWith('#') ? 'group' : 'private',
           groupId: to,
           channelId: to,
           content: text,
-          userId: nick,
-        };
-        const session = new Session(bot.app, data);
-        bot.supportV2PluginSession(session);
-        this.dispatch(session);
-      });
-      bot.status = 0;
-    });
+          userId: nick
+        }
+        const session = new Session(bot.app, data)
+        bot.supportV2PluginSession(session)
+        this.dispatch(session)
+      })
+      bot.status = 0
+    })
   }
 }
 
 // 注册适配器
-Adapter.types.irc = IRCAdapter;
+Adapter.types.irc = IRCAdapter

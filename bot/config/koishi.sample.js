a051f9f97cc099a04e9e839530cfa5e0a06d60e3
arily <lbsaaaaa@gmail.com>
2021-03-09 23:16:54 +0900

    fixed

--- bot/config/koishi.sample.js
+++ bot/config/koishi.sample.js
@@ -1,13 +1,12 @@
 let logLevel
 if (process.env.NODE_ENV !== 'production') logLevel = 0
-module.exports = [{
-  type: 'cqhttp:ws',
+module.exports = {
   port: 7070,
-  server: 'http://localhost:6700',
-  name: '小阿日',
-  commandPrefix: ['!', '！'],
-  logFilter: {
-    CabbageReaction: 3
-  },
-  logLevel
-}]
+  bots: [{
+    type: 'cqhttp:ws',
+    server: 'http://localhost:6700',
+    name: '小阿日',
+    commandPrefix: ['!', '！'],
+    logLevel
+  }]
+}

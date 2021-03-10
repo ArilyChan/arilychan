196bd63fbe967e6dc2511d3927eba02f34fc41d1
arily <lbsaaaaa@gmail.com>
2021-03-11 02:28:11 +0900

    move v2 support to plugin wrapper & split wrapper from framework

--- bot/config/koishi.sample.js
+++ bot/config/koishi.sample.js
@@ -2,11 +2,11 @@ let logLevel
 if (process.env.NODE_ENV !== 'production') logLevel = 3
 module.exports = {
   port: 7070,
+  logLevel,
   bots: [{
     type: 'cqhttp:ws',
     server: 'http://localhost:6700',
     name: '小阿日',
     commandPrefix: ['!', '！'],
-    logLevel
   }]
 }

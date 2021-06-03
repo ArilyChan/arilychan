86f412f2b666738278584c25caa7e4769d4151d9
arily <arily@arily.moe>
2021-06-04 04:19:03 +0900

    puppteer cluster

---
+++ plugins/koishi-plugin-puppeteer-cluster/index.d.ts
@@ -0,0 +1,62 @@
+import { LaunchOptions, Page, ScreenshotOptions, NavigationOptions, Viewport } from 'puppeteer';
+import { ConcurrencyImplementationClassType } from 'puppeteer-cluster/dist/concurrency/ConcurrencyImplementation';
+interface ClusterOptions {
+    concurrency: number | ConcurrencyImplementationClassType;
+    maxConcurrency: number;
+    workerCreationDelay: number;
+    puppeteerOptions: LaunchOptions;
+    perBrowserOptions: LaunchOptions[] | undefined;
+    monitor: boolean;
+    timeout: number;
+    retryLimit: number;
+    retryDelay: number;
+    skipDuplicateUrls: boolean;
+    sameDomainDelay: number;
+    puppeteer: any;
+}
+import { Cluster } from 'puppeteer-cluster'
+import { Context } from 'koishi-core'
+type puppeteerCluster = {
+  instance: Cluster<string, number>,
+  options: {
+    viewport?: Partial<Viewport>,
+    screenshot?: Partial<ScreenshotOptions>,
+    navigation?: Partial<NavigationOptions>
+  }
+  defaultCallback: ({url: string, ...options}) => Promise<any>
+  utils: {
+    wait: (ms: number) => Promise<void>
+    screenshot: (data: {page: Page, data: { url: string } & Partial<puppeteerCluster['options']>}) => Promise<string | Buffer>
+  }
+  screenshot: {
+    base64: (url: string, options: puppeteerCluster['options']) => Promise<string>,
+    binary: (url: string, options: puppeteerCluster['options']) => Promise<Buffer>,
+    save: (url: string, path: string) => Promise<void>
+  }
+}
+
+type Options = {
+  cluster?: {
+    launch?: Partial<ClusterOptions>,
+  }
+  viewport?: Partial<Viewport>,
+  navigation?: Partial<NavigationOptions>
+}
+
+declare module 'koishi-core' {
+  interface Context {
+    puppeteerCluster: puppeteerCluster
+  }
+  namespace Plugin {
+    interface Packages {
+      'koishi-plugin-puppeteer-cluster': typeof import('.')
+    }
+  }
+}
+
+declare namespace plugin {
+  const name: string
+  const apply: (ctx: Context, options: Options) => Promise<void>
+}
+
+export = plugin

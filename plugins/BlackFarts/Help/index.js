ed5191ec2b9b64f241d7a45d6a2026876fe23d85
arily <arily@arily.moe>
2021-12-31 09:50:12 +0900

    fix

--- plugins/BlackFarts/Help/index.js
+++ plugins/BlackFarts/Help/index.js
@@ -2,8 +2,7 @@ const manual = require('sb-bot-manual')
 const cabbage = manual.section('black-farts')
 cabbage.entriesTitle = undefined
 
-let recentFullManual
-let timestamp
+const recent = {}
 async function helps ({ command, meta, app }) {
   // meta.send(helpcontents.join('\n')).catch(e => console.error.bind(console))
   const kw = command.slice(1).join(' ').trim()
@@ -12,10 +11,12 @@ async function helps ({ command, meta, app }) {
     if (!result) return meta.send('没有找到相关词条')
     meta.send(result.renderAsString())
   }
-  if (new Date() - timestamp < 1000 * 60) return meta.send(`[CQ:reply,id=${recentFullManual}] 刚刚发过，很长，往上面翻一翻`)
-  recentFullManual = await meta.send(manual.renderAsString())
-  console.log(recentFullManual)
-  timestamp = new Date()
+  if (new Date() - recent[meta.groupId]?.timestamp < 1000 * 60) return meta.send(`[CQ:reply,id=${recent[meta.groupId].messageId}] 刚刚发过，很长，往上面翻一翻`)
+  await meta.send(manual.renderAsString())
+  recent[meta.groupId] = {
+    messageId: meta.messageId,
+    timestamp: new Date()
+  }
   await meta.send('说明很长。建议使用关键字查询: !help <关键字>')
 }
 module.exports = {

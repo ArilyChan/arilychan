9f46474132cd0412bad4d2e4bd790a9ce7efa9dd
arily <arily@arily.moe>
2022-01-21 20:42:56 +0900

    update CI plugin

--- bot/build.js
+++ bot/build.js
@@ -1,26 +1,5 @@
-// require('dotenv').config()
-// const path = require('path')
-// const appDir = path.dirname(require.main.filename)
-
-// const config = require(`${appDir}/config`)
-
-// const InstallContextPlugin = require('sb-qq-bot-framework/lib/InstallContextPlugin')
-// ;(async () => {
-//   const Installer = new InstallContextPlugin(config.contextPlugins)
-//   await Installer.build()
-//   console.log('build complete')
-//   process.exit(0)
-// })()
-
-const app = require('./init');
-(async () => {
-  try {
-    await app.build()
-    process.exit(0)
-  } catch (error) {
-    console.error(error)
-    process.exit(1)
-  }
-})()
-// app.ci?.build()
-//   .then(() => process.exit(0))
+const app = require('./init')
+app.then(app => 
+  app.ci.build()
+)
+.then(() => process.exit(0))

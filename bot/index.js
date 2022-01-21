ca54fbed0d75a88103cbf9ad84c152160ccd6d07
arily <arily@arily.moe>
2022-01-21 21:16:12 +0900

    refractor start

--- bot/index.js
+++ bot/index.js
@@ -1,2 +1,9 @@
-const app = require('./init')
-app.then(app => app.start())
+const path = require('path')
+const appDir = path.dirname(require.main.filename)
+
+const config = require(`${appDir}/config`)
+const app = require('sb-qq-bot-framework/lib/Bot')(config.koishi)
+
+const init = require('./init')
+init(app)
+  .then(() => app.start())

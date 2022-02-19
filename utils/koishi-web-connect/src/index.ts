4b499a34f4f7185ece5de5947b7b5c05dee9ba72
arily <arily@arily.moe>
2022-02-19 23:52:47 +0900

    please work

--- utils/koishi-web-connect/src/index.ts
+++ utils/koishi-web-connect/src/index.ts
@@ -1,15 +1,35 @@
-import { App } from 'koishi'
-import * as express from 'express'
+import type { IncomingMessage, ServerResponse } from 'http'
+import type { NextFunction } from 'express'
+import type { App } from 'koishi'
 
-export default function connect (app: App) {
-  if (!app?._httpServer) throw new Error('web server not exists. Please check your configuration')
+// eslint-disable-next-line node/no-deprecated-api
+import { parse } from 'url'
 
-  return (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    const baseUrl = req.baseUrl || req.url
-    const exactUrl = req.originalUrl || baseUrl
-    const match = app.router.match(baseUrl, req.method)
+export type ConnectableResuest = IncomingMessage & { originalUrl?: string };
+export type Config = {
+  waitForServer?: boolean
+}
+
+// const doNothing = () => {}
+export function asExpressMiddleware (app: App, { waitForServer }: Config = { waitForServer: false }) {
+  // app.once('ready', doNothing)
+  let wait = Promise.resolve(42)
+  if (waitForServer) {
+    if (!app?.isActive && app?._httpServer) {
+      wait = new Promise<number>((resolve) => {
+        app.once('ready', () => resolve(42))
+      })
+    }
+  } else {
+    if (!app?._httpServer) throw new Error('web server does not exists. Please check your configuration.')
+  }
+
+  return (req: ConnectableResuest, res: ServerResponse, next: NextFunction) => {
+    const exactUrl = req.originalUrl || req.url
+    const matchingUrl = parse(exactUrl as string).pathname || ''
+    const match = app.router.match(matchingUrl, req.method as string)
     if (!match.route) {
-      return next()
+      return wait.then(() => next())
     }
     req.url = exactUrl
     return app?._httpServer?.emit('request', req, res)

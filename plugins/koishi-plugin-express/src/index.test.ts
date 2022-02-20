d06b3fb0493bd6e59eb1412b18f1b717e84134af
arily <arily@arily.moe>
2022-02-21 04:53:17 +0900

    more tests

--- plugins/koishi-plugin-express/src/index.test.ts
+++ plugins/koishi-plugin-express/src/index.test.ts
@@ -1,12 +1,13 @@
 import { App, Context } from 'koishi'
 import request from 'supertest'
-import * as expressService from './index'
+import * as express from './index'
 
 declare module 'supertest' {
   // eslint-disable-next-line no-unused-vars
   interface Test {
     get(url): {
-      text(): string
+      text: string,
+      statusCode: number
     }
   }
 }
@@ -19,11 +20,11 @@ describe('install plugin (not handling defalut router)', () => {
     port: port()
   })
 
-  app.plugin(expressService)
+  app.plugin(express)
   app.plugin({
     name: 'koa router user',
     apply (ctx) {
-      ctx.app.router.use('/koa', ({ response }) => {
+      ctx.app.router.all('/koa', ({ response }) => {
         response.body = 'koa'
       })
     }
@@ -44,5 +45,53 @@ describe('install plugin (not handling defalut router)', () => {
     const res = await request(app._expressHttpServer)
       .get('/express')
     expect(res.text).toEqual('express')
+  })
+
+  it('should not handle koa routes', async () => {
+    const res = await request(app._expressHttpServer)
+      .get('/koa')
+    expect(res.statusCode).toEqual(404)
+  })
+})
+
+describe('use koishi routes', () => {
+  const app = new App({
+    port: port()
+  })
+
+  app.plugin(express, {
+    koishiRoutes: 'use',
+    port: port()
+  })
+  app.plugin({
+    name: 'koa router user',
+    apply (ctx) {
+      ctx.app.router.all('/koa', ({ response }) => {
+        response.body = 'koa'
+      })
+    }
+  })
+
+  app.plugin({
+    name: 'express router user',
+    apply (ctx: Context) {
+      ctx.using(['express'], (ctx) => {
+        ctx.express.get('/express', (req, res) => {
+          res.send('express')
+        })
+      })
+    }
+  })
+
+  it('should handle express routes', async () => {
+    const res = await request(app._expressHttpServer)
+      .get('/express')
+    expect(res.text).toEqual('express')
+  })
+
+  it('should handle koa routes', async () => {
+    const res = await request(app._expressHttpServer)
+      .get('/koa')
+    expect(res.text).toEqual('koa')
   })
 })

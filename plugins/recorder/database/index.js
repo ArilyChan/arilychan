e64cdffe07b1585fa4a7f9edcf3feafed2b33077
arily <arily@arily.moe>
2022-04-28 00:26:22 +0900

    cleanup

--- plugins/recorder/database/index.js
+++
@@ -1,7 +0,0 @@
-const MongoClient = require('mongodb').MongoClient
-
-module.exports = async (options) => {
-  const client = new MongoClient(options.db.uri, { useUnifiedTopology: true })
-  await client.connect()
-  return client.db()
-}

97af48ebe4b31c31f2bd15f6cb5b0ab3dd7f70b2
arily <arily@arily.moe>
2022-05-06 16:48:39 +0900

    remove dev deps in favor of workspace conf

--- plugins/koishi-plugin-express/.eslintrc.js
+++ plugins/koishi-plugin-express/.eslintrc.js
@@ -4,19 +4,7 @@ module.exports = {
     es2021: true,
     'jest/globals': true
   },
-  extends: [
-    'standard'
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
   plugins: [
-    '@typescript-eslint',
     'jest'
-  ],
-  rules: {
-  },
-  root: true
+  ]
 }

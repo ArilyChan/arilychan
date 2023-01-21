6fde8ee1700ea646e84f489822f81fbd0023c15d
arily <arily@arily.moe>
2023-01-21 16:08:09 +0900

    fmt

--- plugins/osu-calendar/src/testRandom.js
+++ plugins/osu-calendar/src/testRandom.js
@@ -1,26 +1,26 @@
-"use strict";
+'use strict'
 
-const run = require('./run');
-const eventPath = './osu-calendar-events.json';
+const run = require('./run')
+const eventPath = './osu-calendar-events.json'
 
 // 模拟meta
 class Meta {
-    constructor(userId) {
-        this.userId = userId;
-    }
+  constructor (userId) {
+    this.userId = userId
+  }
 
-    send(mes) {
-        console.log(mes + "\n");
-    }
+  send (mes) {
+    console.log(mes + '\n')
+  }
 }
 
-let meta = new Meta("1234567890");
-let meta1 = new Meta("1345678902");
-let meta2 = new Meta("2456782427");
-let date = new Date();
+const meta = new Meta('1234567890')
+const meta1 = new Meta('1345678902')
+const meta2 = new Meta('2456782427')
+let date = new Date()
 for (let i = 0; i < 10; i++) {
-    run(meta, eventPath, date);
-    run(meta1, eventPath, date);
-    run(meta2, eventPath, date);
-    date = new Date(date.setDate(date.getDate() + 1))
+  run(meta, eventPath, date)
+  run(meta1, eventPath, date)
+  run(meta2, eventPath, date)
+  date = new Date(date.setDate(date.getDate() + 1))
 }

64d05801cf61676363120c4d05e4614acf06b136
arily <arily@arily.moe>
2023-02-20 12:38:30 +0900

    insp

--- plugins/adapter-mail/src/cutting-edge-mail-client/address/index.ts
+++ plugins/adapter-mail/src/cutting-edge-mail-client/address/index.ts
@@ -1,32 +1,42 @@
 import { MailAddressInterface, LocalMailAddressInterface } from '../../types'
 const customInspectSymbol = Symbol.for('nodejs.util.inspect.custom')
-export class MailAddress<Local extends boolean> implements MailAddressInterface<Local> {
+export class MailAddress implements MailAddressInterface<boolean> {
   name: string = 'unknown'
   address: string
-  local?: Local
+  local?: boolean
   static _printName = 'Mail'
-  constructor ({ name, address, local }: MailAddressInterface<Local>) {
-    this.name = name
+  constructor ({ name, address, local }: MailAddressInterface<boolean>) {
+    this.name = name || this.name
     this.address = address
     this.local = local
   }
 
   [customInspectSymbol] (depth, inspectOptions, inspect) {
     const { stylize } = inspectOptions
-    const formattedAddress = stylize(`${`${stylize(this.name ?? '', 'null')}`}${stylize(`<${this.address}>`, 'string')}`, 'module')
+    const formattedAddress = stylize(`<${`${stylize(this.name ?? '', 'null')}`}${stylize(`<${this.address}>`, 'string')}>`, 'module')
     // @ts-expect-error not so sure about this.
-    return `${stylize(this.constructor._printName, 'special')}\`${formattedAddress}\``
+    return `${stylize(this.constructor._printName, 'special')}${formattedAddress}`
   }
 
   toString () {
     return `${this.name} <${this.address}>`
   }
+
+  extend (e: MailAddressInterface) {
+    return new MailAddress({
+      name: this.name,
+      // @ts-expect-error intended
+      address: this.address,
+      local: this.local,
+      ...e
+    })
+  }
 }
 
-export class LocalMailAddress extends MailAddress<true> implements LocalMailAddressInterface {
+export class LocalMailAddress extends MailAddress implements LocalMailAddressInterface {
   folders
   local: true = true
-  static _printName = 'Mail<Local>'
+  static _printName = 'LocalMail'
   constructor ({ name, address, folders }: MailAddressInterface & Partial<LocalMailAddressInterface>) {
     super({ name, address })
     this.folders = folders

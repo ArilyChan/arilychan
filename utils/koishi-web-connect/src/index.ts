b9ca02673f70d275b0369984ab2cb2e081a6434a
arily <arily@arily.moe>
2023-02-27 12:55:46 +0900

    later

--- utils/koishi-web-connect/src/index.ts
+++ utils/koishi-web-connect/src/index.ts
@@ -2,21 +2,18 @@ import type { IncomingMessage, ServerResponse } from 'http'
 import type { NextFunction, Handler } from 'express'
 import type { App } from 'koishi'
 
-// eslint-disable-next-line node/no-deprecated-api
-import { parse } from 'url'
+import { URL } from 'url'
 
 export type ConnectableResuest = IncomingMessage & { originalUrl?: string };
 export type Config = {
   waitForServer?: boolean
 }
-
-function checkHttpServer (app: App) {
+type Root = App & { _httpServer: any, isActive: boolean }
+function checkHttpServer (app: Root) {
   if (!app?._httpServer) throw new Error('web server does not exists. Please check your configuration.')
 }
 
-// const doNothing = () => {}
-export function asExpressMiddleware (app: App, { waitForServer }: Config = { waitForServer: false }): Handler {
-  // app.once('ready', doNothing)
+export function asExpressMiddleware (app: Root, { waitForServer }: Config = { waitForServer: false }): Handler {
   const logger = app?.logger('web-connect')?.extend('express-middleware') || { ...console, debug: (...args) => console.log('debug:', ...args) }
   let serverOk = false
   if (waitForServer && !app?.isActive) {
@@ -24,6 +21,7 @@ export function asExpressMiddleware (app: App, { waitForServer }: Config = { wai
     app.once('ready', () => {
       checkHttpServer(app)
       serverOk = true
+      logger.info('express server ready')
     })
   } else {
     checkHttpServer(app)
@@ -32,7 +30,7 @@ export function asExpressMiddleware (app: App, { waitForServer }: Config = { wai
 
   return (req: ConnectableResuest, res: ServerResponse, next: NextFunction) => {
     const exactUrl = req.originalUrl || req.url
-    const matchingUrl = parse(exactUrl as string).pathname || ''
+    const matchingUrl = new URL(exactUrl as string).pathname || ''
     const match = app.router.match(matchingUrl, req.method as string)
 
     if (!match.route) {

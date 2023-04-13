2b89482286d12667e5b1bdda6843b1ae08ce0100
arily <arily@arily.moe>
2023-04-13 12:35:39 +0900

    bump & update puppeteer cluster

--- plugins/koishi-plugin-puppeteer-cluster/src/index.ts
+++ plugins/koishi-plugin-puppeteer-cluster/src/index.ts
@@ -3,32 +3,56 @@ import { Page, PuppeteerLifeCycleEvent, ScreenshotOptions, Viewport, WaitForOpti
 import { Context, Schema } from 'koishi'
 import { Cluster } from 'puppeteer-cluster'
 
-const wait = (ms) => new Promise(resolve => setTimeout(resolve, ms))
+type Screenshot = ScreenshotOptions
+  type Config = {
+    cluster: {
+      launch: {
+        concurrency: number,
+        maxConcurrency: number,
+        puppeteerOptions: {
+          headless: boolean,
+          args: string[]
+        }
+      }
+    },
+    viewport: {
+      width: number,
+      height: number,
+      deviceScaleFactor: number
+    },
+    navigation: {
+      waitUntil: PuppeteerLifeCycleEvent
+    }
+  }
+declare module 'koishi' {
+  export interface Context {
+    puppeteerCluster: {
+      instance: Cluster
+      options: {
+        navigation: Config['navigation'],
+        viewport: Config['viewport'],
+        screenshot: Screenshot
+      }
+      _defaultCallback?: () => {}
+      defaultCallback: CallableFunction
+      utils: {
+        wait: (ms: number) => Promise<any>
+        screenshot: (opt: { page: Page, data: { url: string, screenshot: Screenshot, viewport: Config['viewport']} }) => Promise<any>
+      }
+      screenshot: {
+        base64: (url: string | URL, options: ScreenshotOptions) => Promise<string>
+        binary: (url: string | URL, options: ScreenshotOptions) => Promise<Buffer>
+        save: (url: string | URL, options: ScreenshotOptions) => Promise<any>
+      }
+    }
+  }
+}
+const wait = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
 const VIEWPORT = { width: 992, height: 100, deviceScaleFactor: 1.5 }
 
 Context.service('puppeteerCluster')
 export const name = 'koishi-plugin-puppeteer-cluster'
 
-type config = {
-  cluster: {
-    launch: {
-      concurrency: number,
-      maxConcurrency: number,
-      puppeteerOptions: {
-        headless: boolean,
-        args: string[]
-      }
-    }
-  },
-  viewport: {
-    width: number,
-    height: number,
-    deviceScaleFactor: number
-  },
-  navigation: {
-    waitUntil: PuppeteerLifeCycleEvent
-  }
-}
 export const schema = Schema.object({
   cluster: Schema.object({
     launch: Schema.object({
@@ -52,7 +76,7 @@ export const schema = Schema.object({
     waitUntil: Schema.string().default('networkidle0').description('wait until')
   })
 })
-export const apply = async (ctx, options: config) => {
+export const apply = async (ctx: Context, options: Config) => {
   const { cluster: { launch }, viewport, navigation } = options
   const status = {
     cluster: {
@@ -69,36 +93,36 @@ export const apply = async (ctx, options: config) => {
         navigation,
         viewport,
         screenshot: {
-          type: 'png' as const,
-          encoding: 'base64' as const,
+          type: 'png',
+          encoding: 'base64',
           fullPage: true
         }
-      },
+      } as const,
       _defaultCallback: undefined,
-      get defaultCallback () { return c._defaultCallback ?? (ctx.puppeteerCluster as typeof c).utils.screenshot },
+      get defaultCallback () { return c._defaultCallback ?? c.utils.screenshot },
       set defaultCallback (newVal) { c._defaultCallback = newVal },
 
       utils: {
         wait,
         screenshot: async ({ page, data: { url, screenshot, navigation, viewport } = {} }: {
-          page?: Page, data?: {
-            url?: string, screenshot?: ScreenshotOptions, navigation?: WaitForOptions & {
-              referer?: string;
-            }, viewport?: Viewport
-          }
-        }) => {
+            page?: Page, data?: {
+              url?: string, screenshot?: ScreenshotOptions, navigation?: WaitForOptions & {
+                referer?: string;
+              }, viewport?: Viewport
+            }
+          }) => {
           if (!url) return
           await page.setViewport(viewport || c.options.viewport)
           await page.goto(url, navigation || c.options.navigation)
           return await page.screenshot(screenshot || c.options.screenshot)
         }
-      },
+      } as const,
 
       screenshot: {
         base64: (url: string | URL, options: ScreenshotOptions) => cluster.execute({ url, screenshot: { ...c.options.screenshot, encoding: 'base64' }, ...options || {} }, c.utils.screenshot),
         binary: (url: string | URL, options: ScreenshotOptions) => cluster.execute({ url, screenshot: { ...c.options.screenshot, encoding: undefined }, ...options || {} }, c.utils.screenshot),
-        save: (url: string | URL, path: string, options: ScreenshotOptions) => cluster.execute({ url, screenshot: { ...c.options.screenshot, encoding: undefined, path }, ...options || {} }, c.utils.screenshot)
-      },
+        save: (url: string | URL, options: ScreenshotOptions & Pick<Required<ScreenshotOptions>, 'path'>) => cluster.execute({ url, screenshot: { ...c.options.screenshot, encoding: undefined }, ...options || {} }, c.utils.screenshot)
+      } as const,
 
       status
     }
@@ -114,11 +138,11 @@ export const apply = async (ctx, options: config) => {
     throw error
   }
 
-  ctx.before('disconnect', async () => {
+  ctx.on('dispose', async () => {
     if (!status.cluster.inited) return
     if (!status.cluster.injectedToContext) return
-    await ctx.puppeteerCluster.idle()
-    const closeResult = ctx.puppeteerCluster.close().then(res => {
+    await ctx.puppeteerCluster.instance.idle()
+    const closeResult = ctx.puppeteerCluster.instance.close().then(res => {
       delete ctx.puppeteerCluster
       return res
     })

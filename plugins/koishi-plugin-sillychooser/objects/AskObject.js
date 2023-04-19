c84fbb807c33b4ffed8ac4e9350779c5ed6ca774
arily <arily@arily.moe>
2023-04-19 10:53:44 +0900

    no more cqcode

--- plugins/koishi-plugin-sillychooser/objects/AskObject.js
+++ plugins/koishi-plugin-sillychooser/objects/AskObject.js
@@ -29,48 +29,56 @@ class AskObject {
 
   // 将CQCode保存起来并用其他字符替换
   cutCQCode () {
-    this.ask = this.ask.replace(/\[(.+?)\]/g, (matchString, group, index) => {
-      const replacedIndex = this.replaceCQObjects.indexOf(matchString)
-      if (replacedIndex < 0) {
-        const replaceText = '[cqObject' + index + ']'
-        this.replaceCQTexts.push(replaceText)
-        this.replaceCQObjects.push(matchString)
-        return replaceText
-      }
-      return this.replaceCQTexts[replacedIndex]
-    })
+    // TODO no longer supported
+
+    // this.ask = this.ask.replace(/\[(.+?)\]/g, (matchString, group, index) => {
+    //   const replacedIndex = this.replaceCQObjects.indexOf(matchString)
+    //   if (replacedIndex < 0) {
+    //     const replaceText = '[cqObject' + index + ']'
+    //     this.replaceCQTexts.push(replaceText)
+    //     this.replaceCQObjects.push(matchString)
+    //     return replaceText
+    //   }
+    //   return this.replaceCQTexts[replacedIndex]
+    // })
   }
 
   // 将引用保存起来并用其他字符替换
   cutQuote () {
-    this.ask = this.ask.replace(/["'“【「『《](.+?)["'”】」』》]/g, (matchString, group, index) => {
-      const replacedIndex = this.replaceQuoteObjects.indexOf(matchString)
-      if (replacedIndex < 0) {
-        const replaceText = '[quoteObject' + index + ']'
-        this.replaceQuoteTexts.push(replaceText)
-        this.replaceQuoteObjects.push(matchString)
-        return replaceText
-      }
-      return this.replaceQuoteTexts[replacedIndex]
-    })
+    // TODO no longer supported
+
+    // this.ask = this.ask.replace(/["'“【「『《](.+?)["'”】」』》]/g, (matchString, group, index) => {
+    //   const replacedIndex = this.replaceQuoteObjects.indexOf(matchString)
+    //   if (replacedIndex < 0) {
+    //     const replaceText = '[quoteObject' + index + ']'
+    //     this.replaceQuoteTexts.push(replaceText)
+    //     this.replaceQuoteObjects.push(matchString)
+    //     return replaceText
+    //   }
+    //   return this.replaceQuoteTexts[replacedIndex]
+    // })
   }
 
   // 将CQCode替换回去
   reputCQCode (replyMsg) {
-    return replyMsg.replace(/\[(cqObject[0-9]+)\]/g, (matchString) => {
-      const replacedIndex = this.replaceCQTexts.indexOf(matchString)
-      if (replacedIndex < 0) return matchString
-      return this.replaceCQObjects[replacedIndex]
-    })
+    // TODO no longer supported
+    return replyMsg
+    // return replyMsg.replace(/\[(cqObject[0-9]+)\]/g, (matchString) => {
+    //   const replacedIndex = this.replaceCQTexts.indexOf(matchString)
+    //   if (replacedIndex < 0) return matchString
+    //   return this.replaceCQObjects[replacedIndex]
+    // })
   }
 
   // 将引用替换回去
   reputQuote (replyMsg) {
-    return replyMsg.replace(/\[(quoteObject[0-9]+)\]/g, (matchString) => {
-      const replacedIndex = this.replaceQuoteTexts.indexOf(matchString)
-      if (replacedIndex < 0) return matchString
-      return this.replaceQuoteObjects[replacedIndex]
-    })
+    // TODO no longer supported
+    return replyMsg
+    // return replyMsg.replace(/\[(quoteObject[0-9]+)\]/g, (matchString) => {
+    //   const replacedIndex = this.replaceQuoteTexts.indexOf(matchString)
+    //   if (replacedIndex < 0) return matchString
+    //   return this.replaceQuoteObjects[replacedIndex]
+    // })
   }
 
   // 为避免文字后续处理错误，先替换掉特殊格式字符
